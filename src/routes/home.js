fed47b181019c6ba4267fcd8340374ceaf775c36
luantran <toilaluan@github.com>
2023-05-06 10:51:16 +0700

    fix bug

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const homeController = require("../app/controllers/HomeController");
 
 router.get("/", homeController.index);
-router.get('/tag_choose/:tag',homeController.tag_choose)
 
 
 
